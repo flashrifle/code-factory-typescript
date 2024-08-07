4021d62644cbb5385d5e53ca07557ccbf11c3c9d
flashrifle <flashrifle@gmail.com>
2024-08-07 17:29:14 +0900

    chpater3 Type Predicate

---
+++ 3_function/5_type_predicate.js
@@ -0,0 +1,9 @@
+"use strict";
+/*
+    Type Predicate
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+function isNumber(input) {
+    return typeof input === 'number';
+}
+console.log(isNumber('2'));
