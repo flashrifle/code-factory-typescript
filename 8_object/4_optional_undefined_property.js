dc7daef51682125d631c3d2c8ca15f6ed5967378
flashrifle <flashrifle@gmail.com>
2024-08-08 17:24:50 +0900

    chapter8 Optinal vs Undefined 프로퍼티

---
+++ 8_object/4_optional_undefined_property.js
@@ -0,0 +1,19 @@
+"use strict";
+/*
+    Optional and Undefined Property
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const byulE = {
+    name: '별이',
+    age: 12,
+    breed: '미니핀'
+};
+const ori = {
+    name: '오리',
+    age: 12
+};
+const nabi = {
+    name: '나비',
+    age: 7,
+    // breed: undefined
+};
