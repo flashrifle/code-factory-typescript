1194ddc0f0b67e6805be08b982177c18b1e403c3
flashrifle <flashrifle@gmail.com>
2024-08-12 15:06:11 +0900

    chapter11 ConstructorParamters Type

---
+++ 11_uitility_type/10_constructor_parameter_type.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+    Constructor Parameter
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+class Idol {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
